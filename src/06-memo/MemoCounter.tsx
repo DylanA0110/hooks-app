0a25559f785d1621af925ef69b3e8701e5156035
DylanA0110 <dylannjo0606@gmail.com>
2025-09-07 20:14:34 -0600

    add hooks optimization practice

---
+++ src/06-memo/MemoCounter.tsx
@@ -0,0 +1,33 @@
+import { useCounter } from "@/hooks/useCounter";
+import { useMemo } from "react";
+
+const heavyStuff = (iterationNumber: number) => {
+  console.time("Heavy_stuff_started");
+  for (let index = 0; index < iterationNumber; index++) {
+    console.log("Ahi Vamos");
+  }
+  console.timeEnd("Heavy_stuff_started");
+
+  return `${iterationNumber} iteraciones realizadas`;
+};
+
+export const MemoCounter = () => {
+  const { count, increment } = useCounter(40_000);
+  const myHeavyValue = useMemo(()=>heavyStuff(count),[count]);
+  return (
+    <div className="bg-gradient flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Memo - useMemo-{myHeavyValue}</h1>
+      <hr />
+
+      <h4>Counter: {count}</h4>
+      <h4>Counter: {count}</h4>
+
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded-md cursor-pointer"
+        onClick={increment}
+      >
+        +1
+      </button>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 //import { HooksApp } from './HooksApp'
-import './index.css'
+import "./index.css";
+// import { MemoHook } from "./06-memo/MemoHook";
+import { MemoCounter } from "./06-memo/MemoCounter";
 //import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
-import { PokemonPage } from './03-examples/PokemonPage'
-import { TasksApp } from './05-useReducer/TaskApp'
-import { ScrambleWords } from './reducer/ScrambleWords'
+// import { PokemonPage } from './03-examples/PokemonPage'
+// import { TasksApp } from './05-useReducer/TaskApp'
+// import { ScrambleWords } from './reducer/ScrambleWords'
 //import { FocusScreen } from './04-useRef/FocusScreen'
 //import { TrafficLight } from './01-useState/TrafficLight'
 //import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <HooksApp/> */}
     {/* <TrafficLight /> */}
@@ -20,6 +21,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <PokemonPage /> */}
     {/* <FocusScreen/> */}
     {/* <TasksApp/> */}
-    <ScrambleWords/>
-  </StrictMode>,
-)
+    {/* <ScrambleWords/> */}
+    {/* <MemoHook /> */}
+    <MemoCounter/>
+  </StrictMode>
+);
